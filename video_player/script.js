f5a7a4851b252a06859484e9e7b6eab8eb342be1
hasanosman2020 <hasan79.hoa@gmail.com>
2023-07-28 08:12:01 +0100

    feat: code play/pause function

--- video_player/script.js
+++ video_player/script.js
@@ -1,4 +1,26 @@
+const video = document.querySelector('video')
+const progressRange = document.querySelector('.progress_range')
+const progressBar = document.querySelector('.progress_bar')
+const playBtn = document.getElementById('play_btn')
+const volumeIcon = document.getElementById('volume_icon')
+const volumeRange = document.querySelector('.volume_range')
+const volumeBar = document.querySelector('.volume_bar')
+const currentTime = document.querySelector('.time_elapsed')
+const duration = document.querySelector('.time_duration')
+const fullScreen = document.querySelector('.fullscreen')
+
 /***** Play and Pause *****/
+function togglePlay () {
+  if (video.paused) {
+    video.play()
+    playBtn.classList.replace('fa-play', 'fa-pause')
+    playBtn.setAttribute('title', 'Pause')
+  } else {
+    video.pause()
+    playBtn.classList.replace('fa-pause', 'fa-play')
+    playBtn.setAttribute('title', 'Play')
+  }
+}
 
 /***** Progress Bar *****/
 
@@ -7,3 +29,7 @@
 /***** Change Playback Speed *****/
 
 /***** Fullscreen *****/
+
+/***** Event Listeners *****/
+playBtn.addEventListener('click', togglePlay)
+video.addEventListener('click', togglePlay)
