ce5da85e8be8e3aef70df051a83ac1fc649e7045
hasanosman2020 <hasan79.hoa@gmail.com>
2023-07-30 01:51:16 +0100

    feat: populate the current time and duration and complete their functionality

--- video_player/script.js
+++ video_player/script.js
@@ -10,6 +10,14 @@ const duration = document.querySelector('.time_duration')
 const fullScreen = document.querySelector('.fullscreen')
 
 /***** Play and Pause *****/
+
+function showPlayIcon () {
+  if (video.ended) {
+    playBtn.classList.replace('fa-pause', 'fa-play')
+    playBtn.setAttribute('title', 'Play')
+  }
+}
+
 function togglePlay () {
   if (video.paused) {
     video.play()
@@ -17,12 +25,33 @@ function togglePlay () {
     playBtn.setAttribute('title', 'Pause')
   } else {
     video.pause()
-    playBtn.classList.replace('fa-pause', 'fa-play')
-    playBtn.setAttribute('title', 'Play')
+    showPlayIcon()
   }
 }
 
 /***** Progress Bar *****/
+/* Calculate display time format */
+function displayTime (time) {
+  let minutes = Math.floor(time / 60)
+  let seconds = Math.floor(time % 60)
+  /* Make minutes and seconds double digits */
+  minutes = minutes > 9 ? minutes : `0${minutes}`
+  seconds = seconds > 9 ? seconds : `0${seconds}`
+  //console.log('minutes:', minutes, 'seconds:', seconds)
+  return `${minutes}:${seconds}`
+}
+
+/* Update progress bar as video is playing */
+function updateProgress () {
+  //console.log('current time', video.currentTime, 'duration', video.duration)
+  progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`
+  if (video.ended) {
+    progressBar.style.width = 0
+    //displayTime(64)
+  }
+  currentTime.textContent = `${displayTime(video.currentTime)} /`
+  duration.textContent = `${displayTime(video.duration)}`
+}
 
 /***** Volume Controls *****/
 
@@ -33,3 +62,10 @@ function togglePlay () {
 /***** Event Listeners *****/
 playBtn.addEventListener('click', togglePlay)
 video.addEventListener('click', togglePlay)
+
+/* On video end, show play button icon */
+video.addEventListener('ended', showPlayIcon)
+
+/* As video is playing, update progress bar */
+video.addEventListener('timeupdate', updateProgress)
+video.addEventListener('canplay', updateProgress)
